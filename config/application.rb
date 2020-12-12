b013cc2dba3665b48fde0a4add7d567f75c751bc
yamakawa <ippeinogion@yahoo.co.jp>
2020-12-12 01:25:24 +0000

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # ブラウザ側でJavaScriptが無効になっていた場合（Ajaxリクエストが送れない場合）のため認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,16 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
